45ccfaddbaf19ab19dbdd14eb42ae5f07020529e
Danny Kaump <dkaump92@gmail.com>
2022-04-08 04:08:23 -0700

    refactor, style

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
 const drinkName = document.querySelector('h2')
 const img = document.querySelector('img')
 const instructions = document.querySelector('h3')
@@ -11,32 +12,38 @@ document.querySelector('button').addEventListener('click', getDrink)
 
 function getDrink() {
     clear()
-    const drink = input.value
-
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input.value}`;
+    // if empty, randomize
+    if (input.value === '') {url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'}
+    fetch(url)
           .then(res => res.json()) // parse response as JSON
           .then(data => {
-            img.src = data.drinks[0].strDrinkThumb
-            drinkName.innerText = data.drinks[0].strDrink
-            instructions.innerText = data.drinks[0].strInstructions
-            console.log(data)
-            const drinkKeys = Object.keys(data.drinks[0])
-            fillList(drinkKeys, 'strIngredient', data.drinks[0], ingredients)
-            fillList(drinkKeys, 'strMeasure',  data.drinks[0], measurements)
+            const drink = data.drinks[0]
+            const drinkKeys = Object.keys(drink)
+            // update DOM 
+            updateDOM(drink, drinkKeys)
           })
           .catch(err => {
               console.log(`error ${err}`)
           });
 }    
-
-function fillList(obj, keyName, element, dest) {
-    obj.forEach(function(key) {
-        if (key.startsWith(keyName)) {
-            if (element[key] !== null) {
-                dest.innerHTML += `<li>${element[key]}</li>`
+// searches object's keys for values & adds them to appropriate list
+function fillList(keys, keyName, obj, dest) {
+    keys.forEach(function(key) {
+        if (key.startsWith(keyName)) { // *ingredient1, ingredient2, ingredient3....*
+            if (obj[key] !== null) {
+                dest.innerHTML += `<li>${obj[key]}</li>`
             }
         }
     })
+}
+
+function updateDOM(obj, keys) {
+    drinkName.innerText = obj.strDrink
+    img.src = obj.strDrinkThumb
+    fillList(keys, 'strIngredient', obj, ingredients)
+    fillList(keys, 'strMeasure',  obj, measurements)
+    instructions.innerText = obj.strInstructions
 }
 
 function clear() {
